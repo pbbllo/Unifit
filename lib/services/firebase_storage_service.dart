bdb0064258e3f4cb2f189b3777f93d635a710ed1
shiro <shiro@pablo.weslley@ufrpe.br>
2023-11-23 22:45:26 -0300

    feat(models, services) Created services files
    List of Changes:
    - Added requeriments packages in pubspec.yaml
    - Created user model and service files
    - Created Firebase service files
    - Created Error and Log controllers

---
+++ lib/services/firebase_storage_service.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+abstract class IFirebaseStorageService {
+  static final FirebaseStorage firebaseInstance = FirebaseStorage.instance;
+
+  Future get({required String id});
+  Future delete({required String id});
+  Future put({required String id, dynamic value});
+}
+
+class FirebaseStorageService implements IFirebaseStorageService {
+  final FirebaseStorage _firebaseStorage =
+      IFirebaseStorageService.firebaseInstance;
+
+  static final FirebaseStorageService instance = FirebaseStorageService._();
+
+  FirebaseStorageService._();
+
+  @override
+  Future delete({required String id}) async {
+    // TODO: implement delete
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<String> get({required String id}) async {
+    // TODO: implement delete
+    throw UnimplementedError();
+  }
+
+  @override
+  Future put({required String id, dynamic value}) async {
+    // TODO: implement delete
+    throw UnimplementedError();
+  }
+}
