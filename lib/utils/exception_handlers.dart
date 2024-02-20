4d4f52a6e2de0ab2548906fe05acbabd9fe6cb27
Pablo Weslley <pabloweslley2011@gmail.com>
2024-02-20 00:13:33 -0300

    Fixes and Adjusments for sign in flow

--- lib/utils/exception_handlers.dart
+++ lib/utils/exception_handlers.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:unifit/utils/logging.dart';
 import 'package:unifit/utils/constants.dart';
 
@@ -60,6 +61,8 @@ class ExceptionHandler {
         loggedMessage = LoggingStrings.UNDEFINED_ERROR;
     }
     Logging.logWarning(loggedMessage);
+    Fluttertoast.showToast(msg: errorMessage);
+
     return errorMessage;
   }
 
